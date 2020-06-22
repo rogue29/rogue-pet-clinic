7de6e088be3189803dcef4ca8776425a0d876cec
rogue29 <rohitggulyani@gmail.com>
2020-06-22 19:46:35 +0530

    Initialized Spring boot project

---
+++ src/main/java/io/rogue/roguepetclinic/RoguePetClinicApplication.java
@@ -0,0 +1,13 @@
+package io.rogue.roguepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoguePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoguePetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/rogue/roguepetclinic/RoguePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rogue.roguepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoguePetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
