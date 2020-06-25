5c7c2f8bccc7549d255e7bd9004d32da084f655a
rogue29 <rohitggulyani@gmail.com>
2020-06-25 14:33:19 +0530

    Added PetType Service and Map implementation.

---
+++ pet-clinic-data/src/main/java/io/rogue/roguepetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package io.rogue.roguepetclinic.services;
+
+import io.rogue.roguepetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/io/rogue/roguepetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package io.rogue.roguepetclinic.services.map;
+
+import io.rogue.roguepetclinic.model.PetType;
+import io.rogue.roguepetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractServiceMap<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
