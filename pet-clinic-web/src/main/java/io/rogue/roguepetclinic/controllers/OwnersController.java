66350af4970f343a2fd0abb38d2fb9bb1605c000
rogue29 <rohitggulyani@gmail.com>
2020-06-25 17:41:08 +0530

    Added html bindings to controllers.

--- pet-clinic-web/src/main/java/io/rogue/roguepetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/io/rogue/roguepetclinic/controllers/OwnersController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/owners")
+@RequestMapping({"/owners", "/owners.html"})
 public class OwnersController {
     private final OwnerService service;
 
--- pet-clinic-web/src/main/java/io/rogue/roguepetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/io/rogue/roguepetclinic/controllers/VetsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
+@RequestMapping({"/vets", "/vets.html"})
 public class VetsController {
     private final VetService service;
 
